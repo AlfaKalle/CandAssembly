3f1fac55dd25c0d83a5ea504bdffd92a120882bf
Kalle Kuusisalo <143527868+AlfaKalle@users.noreply.github.com>
2023-10-05 19:42:13 +0300

    Improved version with shorter code

--- 3d.c
+++ 3d.c
@@ -88,17 +88,22 @@ void transAndDrawLine(struct Point3d *points, int i1, int i2, int tx, int ty, in
     drawLine(buffer, xx, yy, xx2, yy2, col);
 }
 
+void transAndDrawRectangle(struct Point3d *points, int i1, int i2, int tx, int ty, int tz, int col)
+{
+    int xx = (int)((points[i1].x + tx) / (points[i1].z + tz)) + 160;
+    int yy = (int)((points[i1].y + ty) / (points[i1].z + tz)) + 100;
+
+    int xx2 = (int)((points[i2].x + tx) / (points[i2].z + tz)) + 160;
+    int yy2 = (int)((points[i2].y + ty) / (points[i2].z + tz)) + 100;
+
+    drawRectangle(buffer, xx, yy, xx2, yy2, col);
+}
+
 int main() {
 
-    int done = 0;
-    int x, y, c;
-    unsigned int cc = 0;
-    unsigned int i, t;
     union REGS regs;
-    char character;
     int xx, yy, xx2, yy2;
-    clock_t start, stop;
-    int j;
+    int i, j, t;
 
     int screen_center_x = 160;
     int screen_center_y = 100;
@@ -110,6 +115,7 @@ int main() {
     buffer = malloc(200*320);
     ibuffer = (unsigned int *)buffer;
 
+    /* ns. tien reunapisteet */
     for(i =0;i<POINT_CHALF;i++)
     {
         pointbuffer[i].x = -10000;
@@ -121,35 +127,42 @@ int main() {
         pointbuffer[i + POINT_CHALF].z = 110 + (i * 25);
     }
 
+    /*Laatikon nurkkapisteet järjestyksessä: vasen etu ylä kulma -> vasen taka alakulma*/
     boxpoints[0].x = -1000;
     boxpoints[0].y = -1000;
     boxpoints[0].z =   -50;
 
+    /*oikea etu yläkulma*/
     boxpoints[1].x =  1000;
     boxpoints[1].y = -1000;
     boxpoints[1].z =   -50;
 
+    /*oikea etu alakulma*/
     boxpoints[2].x =  1000;
     boxpoints[2].y =  1000;
     boxpoints[2].z =   -50;
-
+    
+    /*vasen etu alakulma*/
     boxpoints[3].x = -1000;
     boxpoints[3].y =  1000;
     boxpoints[3].z =   -50;
 
-
+    /*vasen taka yläkulma*/
     boxpoints[4].x = -1000;
     boxpoints[4].y = -1000;
     boxpoints[4].z =    50;
 
+    /*oikea taka yläkulma*/
     boxpoints[5].x =  1000;
     boxpoints[5].y = -1000;
     boxpoints[5].z =    50;
 
+    /*oikea taka alakulma*/
     boxpoints[6].x =  1000;
     boxpoints[6].y =  1000;
     boxpoints[6].z =    50;
 
+    /*vasen taka alakulma*/
     boxpoints[7].x = -1000;
     boxpoints[7].y =  1000;
     boxpoints[7].z =    50;
@@ -162,22 +175,14 @@ int main() {
     { 
         fillbuffer(ibuffer, 0);
 
-        for(i=0;i<POINT_CHALF;i++)
-        {
-            xx = (int)((pointbuffer[i].x - j * 100) / pointbuffer[i].z) + screen_center_x;
-            yy = (int)(pointbuffer[i].y / pointbuffer[i].z) + screen_center_y;
-
-            t = i + POINT_CHALF;
-            xx2 = (int)((pointbuffer[t].x - j * 100) / pointbuffer[t].z) + screen_center_x;
-            yy2 = (int)(pointbuffer[t].y / pointbuffer[t].z) + screen_center_y; 
-
-            drawHlinese(buffer, yy, xx, xx2, i + j);         
-        }
-
         for(i=0;i<8;i++)
         {
-            xx = (int)((boxpoints[i].x - j * 100) / (boxpoints[i].z + 250)) + screen_center_x;
-            yy = (int)(boxpoints[i].y / (boxpoints[i].z + 250)) + screen_center_y;
+
+            xx = (int)((boxpoints[i].x - j * 100) / 100) + screen_center_x;
+            yy = (int)(boxpoints[i].y / 100) + screen_center_y;
+
+            xx = (int)((boxpoints[i].x - j * 100) / (boxpoints[i].z + 250)) + screen_center_x; 
+            yy = (int)(boxpoints[i].y / (boxpoints[i].z + 250)) + screen_center_y; 
 
             if(xx >= 0 && xx < 320)
             {
@@ -185,54 +190,50 @@ int main() {
             }
         }
 
-        xx = (int)((boxpoints[4].x - j * 100 - 5000) / (boxpoints[4].z + 250)) + screen_center_x;
-        yy = (int)((boxpoints[4].y - 4000) / (boxpoints[4].z + 250)) + screen_center_y;
+        for(i=0;i<POINT_CHALF;i++)
+        {
+            xx = (int)((pointbuffer[i].x - j * 100) / pointbuffer[i].z) + screen_center_x;
+            yy = (int)(pointbuffer[i].y / pointbuffer[i].z) + screen_center_y;
 
-        xx2 = (int)((boxpoints[6].x - j * 100 - 5000) / (boxpoints[6].z + 250)) + screen_center_x;
-        yy2 = (int)((boxpoints[6].y - 4000) / (boxpoints[6].z + 250)) + screen_center_y;
+        /*    buffer[yy * 320 + xx] = 10; */
 
-        drawRectangle(buffer, xx, yy, xx2, yy2, 10);
+            t = i + POINT_CHALF;
+            xx2 = (int)((pointbuffer[t].x - j * 100) / pointbuffer[t].z) + screen_center_x;
+            yy2 = (int)(pointbuffer[t].y / pointbuffer[t].z) + screen_center_y; 
+
+        /*    buffer[yy2 * 320 + xx2] = 10; */
+
+            drawHlinese(buffer, yy, xx, xx2, i + j);
+        } 
+
+        /*Piirtää vasemman laatikon */
+        transAndDrawRectangle(boxpoints, 4, 6, j * -100 - 5000, -4000, 250, 10);
 
         transAndDrawLine(boxpoints, 0, 4, j * -100 - 5000, -4000, 250, j); 
         transAndDrawLine(boxpoints, 1, 5, j * -100 - 5000, -4000, 250, j); 
         transAndDrawLine(boxpoints, 2, 6, j * -100 - 5000, -4000, 250, j); 
         transAndDrawLine(boxpoints, 3, 7, j * -100 - 5000, -4000, 250, j); 
 
-        xx = (int)((boxpoints[0].x - j * 100 - 5000) / (boxpoints[0].z + 250)) + screen_center_x;
-        yy = (int)((boxpoints[0].y - 4000) / (boxpoints[0].z + 250)) + screen_center_y;
+        transAndDrawRectangle(boxpoints, 0, 2, j * -100 - 5000, -4000, 250, 12); 
 
-        xx2 = (int)((boxpoints[2].x - j * 100 - 5000) / (boxpoints[2].z + 250)) + screen_center_x;
-        yy2 = (int)((boxpoints[2].y - 4000) / (boxpoints[2].z + 250)) + screen_center_y;
-
-        drawRectangle(buffer, xx, yy, xx2, yy2, 11);
-
-
-
-        xx = (int)((boxpoints[4].x - j * 100 + 5000) / (boxpoints[4].z + 250)) + screen_center_x;
-        yy = (int)((boxpoints[4].y - 4000) / (boxpoints[4].z + 250)) + screen_center_y;
-
-        xx2 = (int)((boxpoints[6].x - j * 100 + 5000) / (boxpoints[6].z + 250)) + screen_center_x;
-        yy2 = (int)((boxpoints[6].y - 4000) / (boxpoints[6].z + 250)) + screen_center_y;
-
-        drawRectangle(buffer, xx, yy, xx2, yy2, 10);
+        /*Piirtää oikean laatikon */
+        transAndDrawRectangle(boxpoints, 4, 6, j * -100 + 5000, -4000, 250, 10);
 
         transAndDrawLine(boxpoints, 0, 4, j * -100 + 5000, -4000, 250, j);
         transAndDrawLine(boxpoints, 1, 5, j * -100 + 5000, -4000, 250, j); 
         transAndDrawLine(boxpoints, 2, 6, j * -100 + 5000, -4000, 250, j); 
         transAndDrawLine(boxpoints, 3, 7, j * -100 + 5000, -4000, 250, j); 
 
-        xx = (int)((boxpoints[0].x - j * 100 + 5000) / (boxpoints[0].z + 250)) + screen_center_x;
-        yy = (int)((boxpoints[0].y - 4000) / (boxpoints[0].z + 250)) + screen_center_y;
-
-        xx2 = (int)((boxpoints[2].x - j * 100 + 5000) / (boxpoints[2].z + 250)) + screen_center_x;
-        yy2 = (int)((boxpoints[2].y - 4000) / (boxpoints[2].z + 250)) + screen_center_y;
-
-        drawRectangle(buffer, xx, yy, xx2, yy2, 11);
-
-
+        transAndDrawRectangle(boxpoints, 0, 2, j * -100 + 5000, -4000, 250, 12); 
         
-        buffertovga(ibuffer);
+        buffertovga(ibuffer); 
     }
+
+    regs.h.ah = 0;
+    regs.h.al = 0x12;
+    int86(0x10, &regs, &regs);
+
+    exit(0);
 
     return 0;
 }
